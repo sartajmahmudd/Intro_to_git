d36194a1d0435b97a1235371ffda35df47f0a72a
sartajmahmud <oishik1999@gmail.com>
2024-10-20 21:14:31 +0600

    Base Application for Product Project

---
+++ bin/git_intro.dart
@@ -0,0 +1,14 @@
+
+//Project Product Application
+
+/*
+Features:
+1. Create Category
+2. Read Category
+3. Update Category
+4. Delete Category
+
+ */
+void main(List<String> arguments) {
+  print('Application begins');
+}
---
+++ test/git_intro_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_intro/git_intro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
